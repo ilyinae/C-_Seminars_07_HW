bde4a6b3f1b61a08f5d91e88656a0161fdb5122a
Anton Ilyin <ilyin.a.e@gmail.com>
2022-10-08 12:41:53 +0300

    Added Ex052

---
+++ Ex052/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+double[] CalculateArithmeticMean(int[,] matrix)
+{
+    double[] result = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double summ = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            summ += matrix[i, j];
+        result[j] = Math.Round(summ / (matrix.GetLength(1)), 2);
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("========");
+Console.WriteLine($"{string.Join("\t", CalculateArithmeticMean(matrix))}");
